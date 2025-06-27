9a9e81801390ed05a7c266573a4991ed7572f870
DroneHQio <mtnr.fb@gmail.com>
2025-06-27 11:20:42 -0500

    Cleaned up next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Config options can go here (if needed in the future)
 };
 
 export default nextConfig;
